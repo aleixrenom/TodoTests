7b19a34ddd925f96e8f17c142131432dc2a39f8f
Aleix Renom <aleixrenom@gmail.com>
2020-03-12 14:53:23 +0200

    first push

---
+++ src/TodoTable.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoTable from './TodoTable'
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+
+test('renders todo table', () => {
+	const row = [
+		{desc: 'Go to coffee', date: '24.11.2019'}
+	]
+})
